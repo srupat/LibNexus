657b497d07b16e0cae0ae32b65d4607ed2ccbdd6
srupat <srujanpatwardhan@gmail.com>
2024-03-09 23:30:57 +0530

    feat : section addition halfway through

--- application/controllers.py
+++ application/controllers.py
@@ -1,20 +1,34 @@
 from flask import Flask, request, redirect, url_for
 from flask import render_template, render_template_string
 from flask import current_app as app
-# from application.models import *
+from application.models import *
 from application.forms import ExtendedRegisterForm
 from flask_security import login_required, roles_required
 
 @app.route("/", methods = ["GET", "POST"])
 @login_required
 @roles_required()
-def sections():
+def librarian_home():
     return render_template("lib_dash.html")
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST' and ExtendedRegisterForm(request.form).validate():
-        return redirect(url_for('login'))
+@app.route("/create/section", methods = ["GET", "POST"])
+def sections_home():
+    if request.method == "GET":
+        return render_template("new_sec.html")
+    if request.method == "POST":
+        sec_name = request.form['name']
+        desc = request.form['desc']
+        with app.app_context():
+            try:
+                new_section = Section(sec_name = sec_name, desc = desc)
+                db.session.close_all()
+                db.session.add(new_section)
+                db.session.commit()
+            except:
+                db.session.rollback()
+                return render_template_string("section already exists")
+        sections = db.session.query(Section).to_dict()
+        return render_template("lib_dash.html", sections = sections, sec_name = sec_name, desc = desc)
 
-    return render_template('register.html', form=ExtendedRegisterForm())
+
