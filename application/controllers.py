cba943c7259ef7c67d2ab6127578a3da2177b530
srupat <srujanpatwardhan@gmail.com>
2024-03-10 09:02:15 +0530

    feat: add new section

--- application/controllers.py
+++ application/controllers.py
@@ -4,6 +4,7 @@ from flask import current_app as app
 from application.models import *
 from application.forms import ExtendedRegisterForm
 from flask_security import login_required, roles_required
+from sqlalchemy import select
 
 @app.route("/", methods = ["GET", "POST"])
 @login_required
@@ -28,7 +29,7 @@ def sections_home():
             except:
                 db.session.rollback()
                 return render_template_string("section already exists")
-        sections = db.session.query(Section).to_dict()
+        sections = Section.query.all()
         return render_template("lib_dash.html", sections = sections, sec_name = sec_name, desc = desc)
 
 
